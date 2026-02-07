5d1d5e8b66c5038905c62253807f72c9780209f5
survibo <survibo@gmail.com>
2026-02-07 22:37:24 +0900

    set basic

--- src/pages/StudentCalendar.jsx
+++ src/pages/StudentCalendar.jsx
@@ -5,9 +5,7 @@ import { useMyProfile } from "../hooks/useMyProfile";
 export default function StudentCalendar() {
   const { loading, session, profile } = useMyProfile();
 
-  // ✅ Hook은 조건부 return보다 위에서 "항상" 호출되어야 함
   useEffect(() => {
-    // loading 중엔 굳이 안 돌려도 되면 guard
     if (loading) return;
 
     supabase.auth.getSession().then(console.log);
