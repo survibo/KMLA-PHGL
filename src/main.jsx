1c2a026b7aeede12a696d764beac6a5c180e6153
survibo <survibo@gmail.com>
2026-02-08 11:59:53 +0900

    set basic calendar

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./component.css";
-import "./index.css";
+import "./styles/component.css";
+import "./styles/index.css";
+import "./styles/calendar.css"
+import "./styles/token.css";
+
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
