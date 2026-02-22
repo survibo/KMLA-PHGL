25f238eea35199b19b4423263e3f1f58a1755c8c
survibo <survibo@gmail.com>
2026-02-22 13:18:14 +0900

    analatics 추가

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 import "./styles/index.css";
 
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <App />
       <SpeedInsights />
+      <Analytics/>
     </BrowserRouter>
   </StrictMode>
 );
